a931edc1f280110af23fef8a0f534c330250de52
kyu-suke <ykaneoka7@gmail.com>
2021-05-12 00:15:36 +0900

    statemotifiersample

---
+++ statenotifier_sample/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:statenotifier_sample/counter_state.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StateNotifierProvider<CounterStateNotifier, CounterState>(
+        create: (_) => CounterStateNotifier(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('state_notifier sample'),
+      ),
+      body: Center(
+        child: Text(
+          context.select<CounterState, int>((state) => state.count).toString(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => context.read<CounterStateNotifier>().increment(),
+        label: Text('1'),
+        icon: Icon(Icons.add),
+      ),
+    );
+  }
+}
